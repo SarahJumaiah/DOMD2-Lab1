a43dc06782fcf4325f03db4e5bafe136172811d0
sarahjumaiah <sarahjumaiah@gmail.com>
2024-09-12 11:01:55 +0300

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+
+
+btnunder1.classList.add('btnunder1');
+btnunder2.classList.add('btnunder2');
+btnunder3.classList.add('btnunder3');
+
+
